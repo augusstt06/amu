ebc7862154b2b24ed020128189cd08f3210a6769
augusstt06 <augusstt06@gmail.com>
2025-02-25 17:34:59 +0900

    back: connect db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from .supabase import get_all_restaurants
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
     return {"msg: amu app server is running"}
+
+@app.get('/restaurants')
+def get_restaurants():
+    return get_all_restaurants()
+
+
--- backend/app/supabase.py
+++ backend/app/supabase.py
@@ -8,3 +8,15 @@ url = os.getenv('DB_URL')
 key = os.getenv('DB_KEY')
 
 supabase: Client = create_client(url, key)
+
+def get_all_restaurants():
+    try:
+        query = supabase.table('restaurants').select('*')
+        res = query.execute()
+        return res.data
+    except Exception as e:
+        print("Error details:", str(e))
+        print("Error type:", type(e))
+        import traceback
+        print("Full traceback:", traceback.format_exc())
+        return []
