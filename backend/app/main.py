596285b4494690560992a3d6abf2a3199d64e88f
augusstt06 <augusstt06@gmail.com>
2025-03-07 23:15:10 +0900

    back: api test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from .router.youtube import get_channel_playlist
+
 from app.test import testt
 
 app = FastAPI()
@@ -12,6 +12,3 @@ def read_root():
 async def test():
     return await testt("강남구", 3)
 
-# @app.get('/youtube/playlist')
-# async def get_playlist_videos(channel_name: str, playlist_name: str):
-#     return await get_channel_playlist(channel_name, playlist_name)
