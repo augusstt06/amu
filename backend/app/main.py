0694b9d9bb80e8902e3c2fea0f32eadc195b782f
augusstt06 <augusstt06@gmail.com>
2025-03-21 16:29:23 +0900

    back: get district restaurants analysis api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from backend.app.db.supabase import supabase
 from backend.app.get_restaurants import get_restaurants
+from backend.app.services.api.get_analysis import get_analysis_with_district
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def read_root():
@@ -13,3 +24,10 @@ async def get_restaurants_api(district: str):
     
     return await get_restaurants(district) 
 
+@app.get('/analysis/district/{district}')
+# http://localhost:8000/analysis/district/강남구
+async def get_analysis_api(district: str):
+    
+    analysis = get_analysis_with_district(supabase, district)
+        
+    return [analysis.model_dump() for analysis in analysis]
---
+++ backend/app/services/api/get_analysis.py
@@ -0,0 +1,36 @@
+from typing import Optional, List
+from app.models.analyze import Analyze
+from supabase import Client
+
+def get_analysis_with_district(supabase: Client, district: str) -> List[Analyze]:
+    try:
+        response = supabase.table("analyze")\
+            .select(
+                "*, restaurants!inner(*)"
+            )\
+            .eq("analyze.restaurant_id", "restaurants.id")\
+            .eq("restaurants.district", district)\
+            .execute()
+            
+        if not response.data:
+            return []
+            
+        analysis = []
+        for item in response.data:
+            analyze_data = {
+                'id': item['id'],
+                'restaurant_id': item['restaurant_id'],
+                'name': item['name'],
+                'sentiment_score': item['sentiment_score'],
+                'review_summary': item['review_summary'],
+                'rating_reliability': item['rating_reliability'],
+                'average_rating': item['average_rating'],
+                'created_at': item['created_at']
+            }
+            analysis.append(Analyze(**analyze_data))
+            
+        return analysis
+        
+    except Exception as e:
+        print(f"분석 결과 조회 중 오류 발생: {str(e)}")
+        return []
